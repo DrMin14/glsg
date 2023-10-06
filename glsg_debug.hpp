125977c1d9a1a4cfb89fff40225c9b270c6f76d2
DrMin <83761947+DrMin14@users.noreply.github.com>
2023-10-07 07:39:52 +0900

    Update glsg_debug.hpp

--- glsg_debug.hpp
+++ glsg_debug.hpp
@@ -4,20 +4,14 @@
 #ifdef GLSG_NDEBUG
 #define warning(str)	(void(0))
 
-
-
 #else
 #include <cstdio>
 
 using _debug_log = const char[];
 
-
-
 inline void
 warning(_debug_log _str)
 { puts(_str); }
-
-
 
 #endif
 
